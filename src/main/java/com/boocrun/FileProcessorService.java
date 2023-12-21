e611fca9ea84f2faf6b4417716d06e7fbe8090cd
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-12-20 23:42:12 -0500

    Autowired added

--- src/main/java/com/boocrun/FileProcessorService.java
+++ src/main/java/com/boocrun/FileProcessorService.java
@@ -2,15 +2,17 @@ package com.boocrun;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FileProcessorService {
   
+  @Autowired
   FileReadingService frs;
   
   public void processFile() {
-    frs = new FileReadingService();
+    // frs = new FileReadingService(); // now Autowired
     List<String> lines = frs.readFile("../crimes.csv");
     for (String line : lines) {
       System.out.println(line);
