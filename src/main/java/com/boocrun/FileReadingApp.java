4e49dbceafe1bbdfb5e82f10b01f646f392aed75
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-12-22 23:29:41 -0500

    Created Row Class

--- src/main/java/com/boocrun/FileReadingApp.java
+++ src/main/java/com/boocrun/FileReadingApp.java
@@ -2,12 +2,21 @@ package com.boocrun;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan
+@PropertySource("classpath:application.properties")
 public class FileReadingApp {
+  
+  @Bean
+  static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer () {
+    return new PropertySourcesPlaceholderConfigurer();
+  }
   
   public static void main (String[] args) {
     
